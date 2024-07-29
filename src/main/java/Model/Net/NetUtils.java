4391758ff046e2823a938dbb2a01914978f588bc
Michael Han <mhansnake@gmail.com>
2024-07-29 15:05:55 -0400

    NetUtils and Format base push 1

--- src/main/java/Model/Net/NetUtils.java
+++ src/main/java/Model/Net/NetUtils.java
@@ -1,5 +1,12 @@
 package Model.Net;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
+import Model.Format.Format;
 public class NetUtils {
-Test
+
+
 }
