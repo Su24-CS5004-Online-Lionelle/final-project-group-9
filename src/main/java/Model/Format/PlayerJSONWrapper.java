70d69bb71a320e8d32e10777b8b6e4fcc34c754c
yrankineNEU <rankine.y@northeastern.edu>
2024-08-05 18:51:47 -0400

    fix: updated JSON wrapper
    Signed-off-by: yrankineNEU <rankine.y@northeastern.edu>

--- src/main/java/Model/Format/PlayerJSONWrapper.java
+++ src/main/java/Model/Format/PlayerJSONWrapper.java
@@ -2,16 +2,17 @@ package Model.Format;
 
 import java.util.Collection;
 import Model.IModel;
+import Model.Player;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public final class PlayerJSONWrapper {
   @JsonProperty("data")
-  private Collection<IModel.PlayerBackground> data;
+  private Collection<Player> data;
 
   /**
    * List of records
    */
-  public PlayerJSONWrapper(Collection<IModel.PlayerBackground> players) {
+  public PlayerJSONWrapper(Collection<Player> players) {
     this.data = players;
   }
 }
