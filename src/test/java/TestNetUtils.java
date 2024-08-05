0a00b94c6539f8d1e6ae8da28d1510ddc6ffcb60
yrankineNEU <rankine.y@northeastern.edu>
2024-08-05 18:54:19 -0400

    fix: remove test
    Signed-off-by: yrankineNEU <rankine.y@northeastern.edu>

--- src/test/java/TestNetUtils.java
+++ src/test/java/TestNetUtils.java
@@ -43,19 +43,4 @@ class TestNetUtils {
 //    assertNotEquals(null, backgroundList);
 
   }
-
-  @Test
-  void TestFetchAverages() {
-    // Initialize List of players
-    List<IModel.PlayerAverages> averagesList= new ArrayList<>();
-
-    try {
-      averagesList = NetUtils.fetchSeasonAverages("19");
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-
-
-    assertNotEquals(null, averagesList);
-  }
 }
