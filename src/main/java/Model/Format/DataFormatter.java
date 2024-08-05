6b4c2040b24123d2f4c70e894fbdf4040de76ad3
yrankineNEU <rankine.y@northeastern.edu>
2024-08-05 18:51:26 -0400

    fix: Updated dataformatter to include csv, pretty, and xml
    Signed-off-by: yrankineNEU <rankine.y@northeastern.edu>

--- src/main/java/Model/Format/DataFormatter.java
+++ src/main/java/Model/Format/DataFormatter.java
@@ -3,11 +3,15 @@ package Model.Format;
 import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Collection;
 import java.util.List;
 import Model.Player;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 public class DataFormatter {
 
@@ -23,6 +27,76 @@ public class DataFormatter {
 
     try {
       mapper.writeValue(out, records);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Pretty print the data in a human readable format.
+   *
+   * @param records the records to print
+   * @param out the output stream to write to
+   */
+  private static void prettyPrint(Collection<Player> records, OutputStream out) {
+    PrintStream pout = new PrintStream(out);  // so i can use println
+    for (Player record : records) {
+      prettySingle(record, pout);
+      pout.println();
+    }
+    pout.flush(); // flush all contents to stream
+  }
+
+  /**
+   * Pretty print a single record.
+   *
+   * Let this as an example, so you didn't have to worry about spacing.
+   *
+   * @param record the record to print
+   * @param out the output stream to write to
+   */
+  private static void prettySingle(@Nonnull Player record, @Nonnull PrintStream out) {
+    out.println(record.toString());
+  }
+
+  /**
+   * Write the data as XML.
+   *
+   * @param records the records to write
+   * @param out the output stream to write to
+   */
+  private static void writeXmlData(Collection<Player> records, OutputStream out) {
+
+    // Use DomainXmlWrapper in try catch statement
+    try {
+      ObjectMapper mapper = new XmlMapper();
+      mapper.enable(SerializationFeature.INDENT_OUTPUT);
+      PlayerJSONWrapper wrapper = new PlayerJSONWrapper(records);
+      mapper.writeValue(out, wrapper);
+    } catch (Exception e) {
+      throw new RuntimeException("Writing to XML failed.");
+    }
+  }
+
+
+
+  /**
+   * Write the data as CSV.
+   *
+   * @param records the records to write
+   * @param out the output stream to write to
+   */
+  private static <T> void writeCSVData(Collection<Player> records, OutputStream out) {
+    // Initialize object mapper that will connect format to record
+    CsvMapper mapper = new CsvMapper();
+
+    // Configure CsvSchema to define header structure for CSV file
+    // .withHeader() includes header in file
+    CsvSchema schema = mapper.schemaFor(records.getClass()).withHeader();
+
+    // Write data in try-catch
+    try {
+      mapper.writer(schema).writeValue(out, records);
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -49,11 +123,20 @@ public class DataFormatter {
    * @param format the format to write the records in
    * @param out the output stream to write to
    */
-  public static <T> void write(@Nonnull Collection<T> records, @Nonnull Format format,
+  public static void write(@Nonnull Collection<Player> records, @Nonnull Format format,
                                @Nonnull OutputStream out) {
-
-    writeJsonData(records, out);
+    switch (format) {
+      case XML:
+        writeXmlData(records, out);
+        break;
+      case JSON:
+        writeJsonData(records, out);
+        break;
+      case CSV:
+        writeCSVData(records, out);
+        break;
+      default:
+        prettyPrint(records, out);
+    }
   }
-
-
 }
