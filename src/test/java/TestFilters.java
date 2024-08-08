4f7af34eccbbf0c19745807e0f4284f9162a1f57
yrankineNEU <rankine.y@northeastern.edu>
2024-08-07 22:01:51 -0400

    fix(Filter): updated contains logic for height (in filter)
    Signed-off-by: yrankineNEU <rankine.y@northeastern.edu>

--- src/test/java/TestFilters.java
+++ src/test/java/TestFilters.java
@@ -1,11 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
-import Model.IModel;
-import Model.Model;
 import Model.Player;
 import Model.SortFilter.ColumnData;
 import Model.SortFilter.Filters;
 import Model.SortFilter.Operations;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestFilters {
@@ -103,26 +100,26 @@ class TestFilters {
    */
   @Test
   void getFilterLastNameHappyCase() {
-    boolean actualOne = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.EQUALS, "Seth");
-    assertEquals(true, actualOne); // Seth == SETH
+    boolean actualOne = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.EQUALS, "Beal");
+    assertEquals(false, actualOne); // Curry == Beal
 
     boolean actualTwo = Filters.getFilter(testPlayer3, ColumnData.LAST_NAME, Operations.NOT_EQUALS, "Gilgeous-Alexander");
-    assertEquals(true, actualTwo); // Shai != Gilgeous-Alexander
+    assertEquals(false, actualTwo); // Gilgeous-Alexander != Gilgeous-Alexander is false
 
-    boolean actualThree = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.CONTAINS, "set");
-    assertEquals(true, actualThree); // Seth ~= set
+    boolean actualThree = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.CONTAINS, "-Alex");
+    assertEquals(false, actualThree); // "Curry" ~= set
 
-    boolean actualFour = Filters.getFilter(testPlayer4, ColumnData.LAST_NAME, Operations.GREATER_THAN_EQUALS, "Adam");
-    assertEquals(true, actualFour); // Bradley >= Adam
+    boolean actualFour = Filters.getFilter(testPlayer4, ColumnData.LAST_NAME, Operations.GREATER_THAN_EQUALS, "Doncic");
+    assertEquals(false, actualFour); // Beal >= Doncic
 
-    boolean actualFive = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.GREATER_THAN, "Adam");
-    assertEquals(true, actualFive); // Seth > Adam
+    boolean actualFive = Filters.getFilter(testPlayer2, ColumnData.LAST_NAME, Operations.GREATER_THAN, "Collins");
+    assertEquals(true, actualFive); // Doncic > Collins
 
-    boolean actualSix = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.LESS_THAN, "Stephen");
-    assertEquals(true, actualSix); // Seth < Stephen
+    boolean actualSix = Filters.getFilter(testPlayer5, ColumnData.LAST_NAME, Operations.LESS_THAN, "Grant");
+    assertEquals(true, actualSix); // Collins < Grant
 
-    boolean actualSeven = Filters.getFilter(testPlayer4, ColumnData.LAST_NAME, Operations.LESS_THAN_EQUALS, "Zach");
-    assertEquals(true, actualSeven); // Bradley <= Zach
+    boolean actualSeven = Filters.getFilter(testPlayer6, ColumnData.LAST_NAME, Operations.LESS_THAN_EQUALS, "James");
+    assertEquals(true, actualSeven); // Grant <= James
   }
 
   /**
@@ -131,23 +128,23 @@ class TestFilters {
   @Test
   void getFilterLastNameEdgeCase() {
     // Test case-sensitivity
-    boolean actualOne = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.EQUALS, "SETH");
+    boolean actualOne = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.EQUALS, "curRY");
     assertEquals(true, actualOne); // Seth == SETH
 
-    boolean actualTwo = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.EQUALS, "sEtH");
+    boolean actualTwo = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.EQUALS, "CURRY");
     assertEquals(true, actualTwo); // Seth == SETH
 
     // Test single letter
-    boolean actualThree = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.CONTAINS, "E");
-    assertEquals(true, actualThree); // Seth ~= set
+    boolean actualThree = Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.CONTAINS, "Y");
+    assertEquals(true, actualThree); // Curry ~= Y
 
     // Test equals in greater than equals
-    boolean actualFour = Filters.getFilter(testPlayer4, ColumnData.LAST_NAME, Operations.GREATER_THAN_EQUALS, "Bradley");
-    assertEquals(true, actualFour); // Bradley >= Adam
+    boolean actualFour = Filters.getFilter(testPlayer5, ColumnData.LAST_NAME, Operations.GREATER_THAN_EQUALS, "Beal");
+    assertEquals(true, actualFour);
 
     // Test null
     assertThrows(NullPointerException.class, () -> {
-      Filters.getFilter(testPlayer1, ColumnData.LAST_NAME, Operations.GREATER_THAN, null);
+      Filters.getFilter(testPlayer6, ColumnData.LAST_NAME, Operations.GREATER_THAN, null);
     });
 
     // Empty string
@@ -155,17 +152,66 @@ class TestFilters {
     assertEquals(false, actualSix); // Seth ~= ""
 
     // White space
-    boolean actualSeven = Filters.getFilter(testPlayer4, ColumnData.LAST_NAME, Operations.EQUALS, "   Bradley ");
-    assertEquals(true, actualSeven); // Bradley == Bradley
+    boolean actualSeven = Filters.getFilter(testPlayer5, ColumnData.LAST_NAME, Operations.EQUALS, "   cOllins   ");
+    assertEquals(true, actualSeven);
   }
 
+  /**
+   * Happy getFilter() test cases for height.
+   */
   @Test
-  void getFilterHeight() {
-    boolean expected = Filters.getFilter(testPlayer1, ColumnData.HEIGHT, Operations.GREATER_THAN, "6-2");
-    assertEquals(false, expected);
+  void getFilterHeightHappyCase() {
+    boolean actualOne = Filters.getFilter(testPlayer1, ColumnData.HEIGHT, Operations.EQUALS, "6-4");
+    assertEquals(false, actualOne);
 
-    boolean expected2 = Filters.getFilter(testPlayer3, ColumnData.HEIGHT, Operations.EQUALS, "6-6");
-    assertEquals(true, expected2);
+    boolean actualTwo = Filters.getFilter(testPlayer3, ColumnData.HEIGHT, Operations.NOT_EQUALS, "5-1");
+    assertEquals(true, actualTwo);
+
+    boolean actualThree = Filters.getFilter(testPlayer1, ColumnData.HEIGHT, Operations.CONTAINS, "6-0");
+    assertEquals(false, actualThree);
+
+    boolean actualFour = Filters.getFilter(testPlayer4, ColumnData.HEIGHT, Operations.GREATER_THAN_EQUALS, "6-0");
+    assertEquals(true, actualFour);
+
+    boolean actualFive = Filters.getFilter(testPlayer2, ColumnData.HEIGHT, Operations.GREATER_THAN, "6-10");
+    assertEquals(false, actualFive);
+
+    boolean actualSix = Filters.getFilter(testPlayer5, ColumnData.HEIGHT, Operations.LESS_THAN, "6-10");
+    assertEquals(false, actualSix);
+
+    boolean actualSeven = Filters.getFilter(testPlayer6, ColumnData.HEIGHT, Operations.LESS_THAN_EQUALS, "5-10");
+    assertEquals(false, actualSeven);
+  }
+
+  /**
+   * Happy getFilter() test cases for height.
+   */
+  @Test
+  void getFilterHeightEdgeCase() {
+    // Test trailing spaces
+    boolean actualOne = Filters.getFilter(testPlayer1, ColumnData.HEIGHT, Operations.EQUALS, "  6-1  ");
+    assertEquals(true, actualOne);
+
+    // Test single letter
+    boolean actualThree = Filters.getFilter(testPlayer1, ColumnData.HEIGHT, Operations.CONTAINS, "6");
+    assertEquals(true, actualThree);
+
+    // Test equals in greater than equals
+    boolean actualFour = Filters.getFilter(testPlayer5, ColumnData.HEIGHT, Operations.GREATER_THAN_EQUALS, "6-11");
+    assertEquals(true, actualFour);
+
+    // Test null
+    assertThrows(NullPointerException.class, () -> {
+      Filters.getFilter(testPlayer6, ColumnData.HEIGHT, Operations.GREATER_THAN, null);
+    });
+
+    // Empty string
+    boolean actualSix = Filters.getFilter(testPlayer1, ColumnData.HEIGHT, Operations.LESS_THAN, "");
+    assertEquals(false, actualSix);
+
+    // White space
+    boolean actualSeven = Filters.getFilter(testPlayer5, ColumnData.HEIGHT, Operations.EQUALS, "   6 - 11   ");
+    assertEquals(true, actualSeven);
   }
 
 }
